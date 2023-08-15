70409eca029146891cd5800d80925ac9faede41b
aa-r-ju <aarju3447@gmail.com>
2023-08-15 14:53:41 +0545

    feat:completed 2.3 exercise

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 const Total = ({total}) => {
-let sum = 0;
-for(let i = 0; i<total.length;i++){
-   let currentValue = total[i].exercises
-    sum+=currentValue
-}
+const sum = total.reduce((accumulator,currentValue) =>{
+    return accumulator + currentValue.exercises
+},0)
   return (
     <div>
-    <p> <b>Total of {sum} exercises</b> </p>
+    <p> <b>Total of {sum}  exercises</b> </p>
     </div>
   )
 }
