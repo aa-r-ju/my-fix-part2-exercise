67894dc24e08f77eec654926241ac7fd2fe799b8
aa-r-ju <aarju3447@gmail.com>
2023-08-15 15:13:42 +0545

    feat:completed 2.4 exercise

--- src/App.js
+++ src/App.js
@@ -3,38 +3,72 @@ import Header from "./Header"
 import Course from "./Course"
 import Total from "./Total"
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+       <h1> Web development curriculum </h1>
+  
+  {
+    courses.map((course)=>{
+  
+      return (
+        <div key = {course.id}>
+     <Header header = {course.name}/>
+      <Course course = {course.parts}/>
+      <Total total = {course.parts}/>
+        </div>
+     
+      )
+  
+    })
   }
-  const newValue = {name:"Redux",exercises:11,id:4}
-    course.parts.push(newValue)
-
-
-  return(  <>  
-  <Header header = {course.name} />
-   <Course course={course.parts} /> 
-   <Total total= {course.parts} />
-  </>
-)
-
+  
+  
+   </div>
+  )
 }
-
 export default App
